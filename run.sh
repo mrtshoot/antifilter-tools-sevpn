a287188461b549a9f61d4fbd5eaa11d59fbb9a0b
Hossein Ghorbani <hosi.ghorbani@gmail.com>
2023-03-20 16:21:04 +0330

    upgrade

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Install requirementes
+sudo apt install -y \
+ansible && \
+sshpass
+
+#get insite and outside informations
+read -p "InsideServer IP: " INSIDE_IP
+read -p "InsideServer SSH Password: " INSIDE_SSH_PASS
+read -p "OutsideServer IP: " OUTSIDE_IP
+read -p "OutsideServer SSH Password: " OUTSIDE_SSH_PASS
+read -p "Squidproxy Username: " SQUID_USER
+read -p "Squidproxy Password: " SQUID_PASS
+
+#Set Inside and Outside Servers and create inventory file
+sed -i 's/INSIDE_IP/"${INSIDE_IP}"/g' inventory
+sed -i 's/INSIDE_SSH_PASS/"${INSIDE_SSH_PASS}"/g' inventory
+sed -i 's/OUTSIDE_IP/"${OUTSIDE_IP}"/g' inventory
+sed -i 's/OUTSIDE_SSH_PASS/"${OUTSIDE_SSH_PASS}"/g' inventory
+
+#run ansible command
+ansible-playbook -i inventory playbook.yaml --extra-vars="USERNAME='${SQUID_USER}' PASSWORD='${SQUID_PASS}'"
+
+#OpenVPN Certificate
+ansible -m shell --become -a 'grep "byte ServerCert" /root/antifilter-tools-sevpn/vpn_server.config | cut -d " " -f3 | fold -w 64' -i inventory inside
